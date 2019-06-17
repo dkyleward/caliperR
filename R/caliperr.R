b506d2daa84aec744ea032d6684c7843fe27c99f
Kyle Ward <kyle@caliper.com>
2019-06-17 10:43:12 -0400

    Create connect()

---
+++ R/caliperr.R
@@ -0,0 +1,61 @@
+#'Create a connection to a Caliper software product
+#'
+#'Will create a connection over COM to one of Caliper's installed software
+#'products. (The software must be installed with a valid license.)
+#'
+#'@param software One of either "TransCAD", "TransModeler", or "Maptitude". If
+#'  left \code{NULL}, the function will search (in that order) and create the first
+#'  connection it can.
+#'@import RDCOMClient
+#'@return Nothing. Sets the COM object to a global environment variable
+#'  (\code{caliper_dk})
+#'@export
+
+connect <- function(software = NULL){
+
+  valid_software_values <- c("TransCAD", "TransModeler", "Maptitude")
+  if (!is.null(software)){
+    if (!(software %in% valid_software_values)) {
+      stop(
+        paste0(
+          "(caliperr::connect) Invalid value for 'software'. Valid values are: ",
+          paste(valid_software_values, collapse = ", ")
+        )
+      )
+    }
+  }
+
+  # Try to connect if the user provided a value for `software`
+  if (!is.null(software)) {
+    tryCatch(
+      dk <-  COMCreate(paste0(software, ".AutomationServer")),
+      error = function(c) {
+        c$message <- paste0(
+          "Could not create a connection to ", software, ". ",
+          "Check that ", software, " is installed."
+        )
+        stop(c)
+      }
+    )
+  # If the user didn't provide a value for `software`
+  } else {
+    softwares <- valid_software_values[!is.null(valid_software_values)]
+    for (software in softwares){
+      suppressWarnings(
+        try(
+          dk <-  COMCreate(paste0(software, ".AutomationServer")),
+          silent = TRUE
+        )
+      )
+      if (exists("dk")) break
+    }
+  }
+
+  if (!exists("dk")) stop(paste0(
+    "Could not connect to any Caliper software. ",
+    "Check that one of the following is installed: ",
+    paste(softwares, collapse = ", ")
+  ))
+
+  assign("dk", dk, envir = .GlobalEnv)
+}
