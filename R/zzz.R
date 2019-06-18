1f74f3f0153c660839f4ecacc1e4e8a4d157ffe2
Kyle Ward <kyle@caliper.com>
2019-06-18 09:00:29 -0400

    Improve .onLoad() behavior

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,17 @@
   if (exists("CALIPER_DK", envir = .GlobalEnv)){
     remove("CALIPER_DK", envir = .GlobalEnv)
   }
-  connect()
+  if (exists("CALIPER_SOFTWARE", envir = .GlobalEnv)){
+    remove("CALIPER_SOFTWARE", envir = .GlobalEnv)
+  }
+  if (exists("CALIPER_UI", envir = .GlobalEnv)){
+    remove("CALIPER_UI", envir = .GlobalEnv)
+  }
+  try({
+    connect()
+    software <- get("CALIPER_SOFTWARE", envir = .GlobalEnv)
+  })
+  if (exists("CALIPER_SOFTWARE", envir = .GlobalEnv)) {
+    packageStartupMessage("Connected to ", software)
+  }
 }
