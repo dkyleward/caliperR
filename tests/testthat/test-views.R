664e2b272839cc756d243318a6751bece8e2323c
Kyle Ward <kyle@caliper.com>
2020-01-27 16:36:31 -0500

    Add unit tests

--- tests/testthat/test-views.R
+++ tests/testthat/test-views.R
@@ -33,6 +33,7 @@ test_that("reading a bin file works over COM", {
   expect_equal(typeof(df[[3]]), 'character') # preserve type for null fields
   expect_equal(typeof(df[[5]]), 'double') # preserve type for null fields
   expect_equal(nrow(df), 6) # make sure the deleted record isn't read in.
+  expect_equal(Hmisc::label(df)[[1]], "first field") # field descriptions
 })
 
 test_that("reading a bin file works without COM", {
@@ -47,13 +48,30 @@ test_that("reading a bin file works without COM", {
   expect_equal(typeof(df[[3]]), 'character') # preserve type for null fields
   expect_equal(typeof(df[[5]]), 'double') # preserve type for null fields
   expect_equal(nrow(df), 6) # make sure the deleted record isn't read in.
+  expect_equal(Hmisc::label(df)[[1]], "first field") # field descriptions
   try(connect(), silent = TRUE)
 })
 
-test_that("writing a bin file works", {
+testh_that("reading with and without COM give same data", {
+  check_connected()
+  bin_file <- system.file(
+    "extdata", "gisdk", "testing", "toy_table.bin", package = "caliper"
+  )
+  df1 <- read_bin(bin_file)
   disconnect()
+  df2 <- read_bin(bin_file)
+  expect_true(all(df2 == df, na.rm = TRUE))
+  expect_true(all(unlist(lapply(df1, typeof)) == unlist(lapply(df2, typeof))))
+  try(connect(), silent = TRUE)
+})
 
+bin_file <- system.file(
+  "extdata", "gisdk", "testing", "toy_table.bin", package = "caliper"
+)
+df <- read_bin(bin_file)
 
+test_that("writing a bin file works without COM", {
+  disconnect()
   field_descriptions <- Hmisc::label(df) # check field descriptions
   temp_bin <- tempfile(fileext = ".bin")
   #write_bin(df, temp_bin, dnames = dnames)
@@ -66,4 +84,5 @@ test_that("writing a bin file works", {
   # )
   # df2 <- read_bin(temp_bin)
   # expect_equal(df, df2)
+  try(connect(), silent = TRUE)
 })
