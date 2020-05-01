62dfb66508f4823a3149b6d2907a5975532b9389
Kyle Ward <kyle@caliper.com>
2020-05-01 16:27:31 -0400

    Document the CaliperR class

--- R/CaliperR-class.R
+++ R/CaliperR-class.R
@@ -1,3 +1,12 @@
+# © Copyright Caliper Corporation. Licensed under Apache License 2.0.
+
+#' R class that gives access to GISDK functions
+#'
+#' This class makes it easy to call any arbitrary GISDK function from R.
+#'
+#' @import R6
+#' @export
+
 CaliperR <- R6::R6Class("CaliperR",
   public = list(
     initialize = function() {
@@ -10,10 +19,29 @@ CaliperR <- R6::R6Class("CaliperR",
     },
     CreateObject = function(class_name, ...) {
       caliperR::CreateObject(class_name, ...)
+    },
+    SetAlternateInterface = function(ui_file = NULL) {
+      caliperR::SetAlternateInterface(ui_file)
+    },
+    GetInterface = function() {
+      caliperR::GetInterface()
     }
   )
 )
 
+#' S3 method for calling \code{CaliperR} object methods
+#'
+#' Makes \code{CaliperR} objects smarter about whether you are calling a
+#' method from the R object or the underlying GISDK library.
+#'
+#' @details
+#'
+#' If \code{name} is a method of the R object, then it is executed. Otherwise,
+#' it tries to execute a GISDK function of the same name.
+#'
+#' @param x A \code{CaliperR} object
+#' @param name the method to dispatch
+#' @export
 
 `$.CaliperR` <- function(x, name, ..., process_result = TRUE) {
   args <- list(...)
@@ -28,7 +56,7 @@ CaliperR <- R6::R6Class("CaliperR",
   }
 }
 
-# dk <- CaliperR$new()
+# dk <- connect()
 # folder <- dk$RunMacro("G30 Tutorial Folder")
 # view = dk$OpenTable("airports", "ffb", list(paste0(folder, "\\airports.bin")))
 # dk$ShowArray(list("a", "b"))
